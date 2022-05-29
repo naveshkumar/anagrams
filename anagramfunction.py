e17d37c6488d07ba055d96a8e098520d21672eb2
naveshkumar <47080512+naveshkumar@users.noreply.github.com>
2022-05-29 11:00:33 +0100

    update with anagramer

--- anagramfunction.py
+++ anagramfunction.py
@@ -1,7 +1,25 @@
+#get the english word list
+
+englistfile = open("words.txt","r")
+loklist_i = []
+loklist_i = englistfile.readlines()
+loklist = []
+for word in loklist_i:
+    loklist.append(word.strip())
+
+#group the english word list in a dictionary by length of the words
+
+engdict = {loklist[i]:len(loklist[i]) for i in range(0,len(loklist))}
+lendict = {}
+for key,val in engdict.items():
+    lendict[val] = [key] if val not in lendict.keys() else lendict[val] + [key]
+
+# Anagram function 1 : list of word concatanated into a single word
 def list_to_word(lst):
     word = "".join(lst)
     return(word)
 
+# Anagram function 2 : function that does the swapping of words on the same principle as nPr calculation
 def swapper(wchar,wlen):
     initlist = []
     for step in range(1,wlen):
@@ -18,6 +36,8 @@ def swapper(wchar,wlen):
     initlist = list(set(initlist))
     return(initlist)
 
+
+# Anagram function 3 : get inital values and then create combinations further still to get the final complete jumble
 def permutate(word):
     
     allnpn = [word]
@@ -33,3 +53,12 @@ def permutate(word):
             
     allnpn = list(set(allnpn))
     return(allnpn)
+
+#the main function
+
+def anagramer(word):
+    anagrams = []
+    allcombo = set(permutate(word))
+    lokset = set(lendict[len(word)])
+    anagrams = lokset.intersection(allcombo)
+    return(anagrams)
